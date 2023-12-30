475cc5affc411004b4fa5d6eab3b19638ea3dcfc
AsynchronousMatrix <Catbob614@gmail.com>
2023-12-30 17:53:54 +0000

    feature: update 'lint-check' CI

--- .lune/lint-check.lua
+++ .lune/lint-check.lua
@@ -1,21 +1,26 @@
 --# selene: allow(incorrect_standard_library_use)
 
 local process = require("@lune/process")
-
-local function execute(application, ...)
-	local result = process.spawn(application, { ... })
-
-	if not result.ok then
-		print(`Executing application '{application}' failed: '{result.stderr}'`)
-
-		return process.exit(result.code)
-	end
-
-	return result.stdout
-end
+local fileSystem = require("@lune/fs")
 
 local function main()
-	execute("selene", "lint", "Modules")
+	for _, packageName in fileSystem.readDir("Modules") do
+		local result = process.spawn(`selene`, {
+			`.`,
+		}, {
+			cwd = `{process.cwd}Modules/{packageName}`,
+		})
+
+		if result.ok then
+			print(`[Lint-Projects]: project '{packageName}' selene lint OK`)
+		else
+			print(
+				`[Lint-Projects]: project '{packageName}' selene lint FAIL ({result.code}):\n{result.stderr}\n{result.stdout}`
+			)
+
+			process.exit(result.code)
+		end
+	end
 end
 
 return main()
